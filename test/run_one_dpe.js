0d5b52619177c9d60b9318d94ef409a2d3dbc97f
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2024-10-31 13:26:30 +0100

    Deactivate bug compat

--- test/run_one_dpe.js
+++ test/run_one_dpe.js
@@ -6,7 +6,7 @@ import enums from '../src/enums.js';
 import { calcul_3cl } from '../src/engine.js';
 import { set_bug_for_bug_compat } from '../src/utils.js';
 
-set_bug_for_bug_compat();
+//set_bug_for_bug_compat();
 const args = process.argv.slice(2);
 const dpe_json_file = args[0];
 fs.readFile(dpe_json_file, 'utf8', (err, data) => {
